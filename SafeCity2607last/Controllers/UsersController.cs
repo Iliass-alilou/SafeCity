358e6de79b52f29784719779fc59aec7fbbc142a
iliass alilou <iliassalilou18@gmail.com>
2020-08-14 20:56:57 +0100

    Users/update

--- SafeCity2607last/Controllers/UsersController.cs
+++ SafeCity2607last/Controllers/UsersController.cs
@@ -90,39 +90,22 @@ namespace SafeCity2607last.Controllers
 
 
         [HttpPost]
-        //public async Task<IActionResult> Update(string id, string email, string password
-                    public async Task<IActionResult> Update
+       
+        public async Task<IActionResult> Update
             (string id,
             string email,
             string password,
+            string ConfirmPassword,
             string FirstName,
             string LastName,
-            string PhoneNumber)
-       
-        // string FirstName,
-        //string LastName,
-        //string Function,
-
-        //string CIN,
-
-        //string Adresse,
-
-        //string City,
-
-        //string PhoneNumber1,
-
-
-        //string PhoneNumber2,
-
-        //string photo,
-
-
-
-        //string Lati,
-
-
-        //string Long)
-        {
+            string PhoneNumber,
+            string City,
+            string Adresse,
+            string PhoneNumber2)
+ 
+            //string Lati,
+            //string Long)
+            {
             ApplicationUser user = await userManager.FindByIdAsync(id);
             if (user != null) 
             {
@@ -132,12 +115,50 @@ namespace SafeCity2607last.Controllers
                 else
                     ModelState.AddModelError("", "Email cannot be empty");
 
-                if (!string.IsNullOrEmpty(password))
-                    user.PasswordHash = passwordHasher.HashPassword(user, password);
-                else
-                    ModelState.AddModelError("", "Password cannot be empty");
+                /* if (!string.IsNullOrEmpty(password))
+                     user.PasswordHash = passwordHasher.HashPassword(user, password);
+                 else
+                     ModelState.AddModelError("", "Password cannot be empty");*/
 
-                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+
+                if (!string.IsNullOrEmpty(FirstName))
+                    user.FirstName = FirstName;
+                else
+                    ModelState.AddModelError("", "First Name cannot be empty");
+
+                if (!string.IsNullOrEmpty(LastName))
+                    user.LastName = LastName;
+                else
+                    ModelState.AddModelError("", "Last Name cannot be empty");
+
+
+                if (!string.IsNullOrEmpty(PhoneNumber))
+                    user.LastName = PhoneNumber;
+                else
+                    ModelState.AddModelError("", "Phone Number1 cannot be empty");
+
+
+
+                if (!string.IsNullOrEmpty(City))
+                    user.LastName = City;
+                else
+                    ModelState.AddModelError("", "City cannot be empty");
+
+
+                if (!string.IsNullOrEmpty(Adresse))
+                    user.LastName = Adresse;
+                else
+                    ModelState.AddModelError("", "Adresse cannot be empty");
+
+
+                if (!string.IsNullOrEmpty(PhoneNumber2))
+                    user.LastName = PhoneNumber2;
+                else
+                    ModelState.AddModelError("", "Phone Number2 cannot be empty");
+
+
+
+                if (!string.IsNullOrEmpty(email) /*&& !string.IsNullOrEmpty(password)*/)
                 {
                     IdentityResult result = await userManager.UpdateAsync(user);
                     if (result.Succeeded)
@@ -146,36 +167,7 @@ namespace SafeCity2607last.Controllers
                         Errors(result);
                 }
 
-                //if (!string.IsNullOrEmpty(FirstName))
-                //    user.FirstName = FirstName;
-                //else
-                //    ModelState.AddModelError("", "FirstName cannot be empty");
-
-                //if (!string.IsNullOrEmpty(LastName))
-                //    user.LastName = LastName;
-                //else
-                //    ModelState.AddModelError("", "LastName cannot be empty");
-
-                //if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
-                //{
-                //    IdentityResult result = await userManager.UpdateAsync(user);
-                //    if (result.Succeeded)
-                //        return RedirectToAction("Index");
-                //    else
-                //        Errors(result);
-                //}
-
-
-
-
-
-
-
-
-
-
-
-
+               
 
             }
             else
@@ -195,95 +187,3 @@ namespace SafeCity2607last.Controllers
 
 
 
-//using System;
-//using System.Threading.Tasks;
-//using WebAppPLatForm2309100Ex.Models;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-
-
-//// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-//namespace WebAppPLatForm2309100Ex.Controllers
-//{
-
-
-//    [Authorize(Roles="SuperAdmin")]
-//    public class UsersController : Controller
-//    {
-//        private UserManager<ApplicationUser> userManager;
-//        private IPasswordHasher<ApplicationUser> passwordHasher;
-
-//        public UsersController(UserManager<ApplicationUser> usrMgr, IPasswordHasher<ApplicationUser> passwordHash)
-//        {
-//            userManager = usrMgr;
-//            passwordHasher = passwordHash;
-//        }
-
-//        // GET: /<controller>/
-//        public IActionResult Index()
-//        {
-//            return View(userManager.Users);
-//        }
-//        public async Task<IActionResult> Update(string id)
-//        {
-//            ApplicationUser user = await userManager.FindByIdAsync(id);
-//            if (user != null)
-//                return View(user);
-//            else
-//                return RedirectToAction("Index");
-//        }
-
-//        [HttpPost]
-//        public async Task<IActionResult> Update(string id, string email, string password)
-//        {
-//            ApplicationUser user = await userManager.FindByIdAsync(id);
-//            if (user != null)
-//            {
-//                if (!string.IsNullOrEmpty(email))
-//                    user.Email = email;
-//                else
-//                    ModelState.AddModelError("", "Email cannot be empty");
-//                if (!string.IsNullOrEmpty(password))
-
-//                    user.PasswordHash = passwordHasher.HashPassword(user, password);
-//                else
-//                    ModelState.AddModelError("", "Password cannot be empty");
-
-//                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
-//                {
-//                    IdentityResult result = await userManager.UpdateAsync(user);
-//                    if (result.Succeeded)
-//                        return RedirectToAction("Index");
-//                    else
-//                        Errors(result);
-//                }
-//            }
-//            else
-//                ModelState.AddModelError("", "User Not Found");
-//            return View(user);
-//        }
-
-//        private void Errors(IdentityResult result)
-//        {
-//            foreach (IdentityError error in result.Errors)
-//                ModelState.AddModelError("", error.Description);
-//        }
-//        public async Task<IActionResult> Delete(string id)
-//        {
-//            ApplicationUser user = await userManager.FindByIdAsync(id);
-//            if (user != null)
-//            {
-//                IdentityResult result = await userManager.DeleteAsync(user);
-//                if (result.Succeeded)
-//                    return RedirectToAction("Index");
-//                else
-//                    Errors(result);
-//            }
-//            else
-//                ModelState.AddModelError("", "User Not Found");
-//            return View("Index", userManager.Users);
-//        }
-//    }
-//}
